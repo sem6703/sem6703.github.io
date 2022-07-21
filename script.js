75c531488e4c0457c1922e301d515a7df9dece31
sem6703 <sem6703@yandex.ru>
2022-07-21 16:31:42 +0300

    great complite

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {//моб меню
-const mobs=document.querySelectorAll('.mob li, .mmm');
-const mob=document.querySelector('.mob');
+const mobs=document.querySelectorAll('.mobile li, .mmm');
+const mob=document.querySelector('.mobile');
 let tm=null;
 const mmm=()=>{
 	mob.classList.toggle('vid');
@@ -42,70 +42,76 @@ lng.forEach(x=>{x.addEventListener('click',
 })();
 
 (function (){//slider
-const feb2=(x)=>{// создание слайдера
+const slider=(x)=>{
 	x.innerHTML=`
-	<button class="pointer" style="width:42px;height:42px;background: transparent;border: none;padding:3px 0 0 0px"> 
-	<svg width="30" height="23" viewBox="0 0 30 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M27.8106 10.6252H7.32406L14.1008 3.93617L12.6412 2.46973L4.90009 10.1107L3.41442 11.5771L4.90009 13.0178L12.6412 20.6588L14.1008 19.1923L7.50651 12.6834H27.8106V10.6252Z" fill="#214E41"/>
-	</svg>
+	<button class="brands__prev pointer"> 
+		<img src="img/arrowprev.svg" alt="arrow prev">
 	</button>
 
-	<div style="width:1046px;height:100px;border:0px solid black;overflow:hidden;
-	margin-right:8px;">
-		<div style="width:3273px; height:100px;background: white;
-		display:flex;justify-content:space-between;align-items: center;">
-	<img src="img/pic8.png"><img src="img/pic9.png"><img src="img/pic10.png">
-	<img src="img/pic11.png"><img src="img/pic12.png" style="margin-left:14px; margin-right:14px">
-	<img src="img/pic8.png"><img src="img/pic9.png"><img src="img/pic10.png">
-	<img src="img/pic11.png"><img src="img/pic12.png" style="margin-left:14px; margin-right:14px">
-	<img src="img/pic8.png"><img src="img/pic9.png"><img src="img/pic10.png">
-	<img src="img/pic11.png"><img src="img/pic12.png" style="margin-left:14px; margin-right:14px">
+	<div class="brands__window">
+		<div class="brands__tape">
+			<img src="img/pic8.png">
+			<img src="img/pic9.png">
+			<img src="img/pic10.png">
+			<img src="img/pic11.png">
+			<img src="img/pic12.png" style="margin-left:14px; margin-right:14px">
+			<img src="img/pic8.png">
+			<img src="img/pic9.png">
+			<img src="img/pic10.png">
+			<img src="img/pic11.png">
+			<img src="img/pic12.png" style="margin-left:14px; margin-right:14px">
+			<img src="img/pic8.png">
+			<img src="img/pic9.png">
+			<img src="img/pic10.png">
+			<img src="img/pic11.png">
+			<img src="img/pic12.png" style="margin-left:14px; margin-right:14px">
 		</div>
 	</div>
 
-	<button class="pointer" style="width:42px;height:42px;background: none;border: none;padding:3px 0 0 3px"> 
-	<svg width="30" height="23" viewBox="0 0 30 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M3.41443 12.5034H23.901L17.1242 19.1924L18.6099 20.6589L26.3249 13.0179H26.351L27.8106 11.5772L26.351 10.1108H26.3249L18.6099 2.46985L17.1242 3.93629L23.7446 10.4452H3.41443V12.5034Z" fill="#214E41"/>
-	</svg>
+	<button class="brands__next pointer"> 
+		<img src="img/arrownext.svg" alt="arrow next">
 	</button>
 	`;
 
-	x.setAttribute('style','display:flex;justify-content: space-between;align-items: center;');
+	x.setAttribute('style','display:flex;');
+	x.style.justifyContent='space-between';
+	x.style.alignItems='center';
 	x.style.width = '100%';
+	//x.style.border = '5px solid blue';
 
-	const lenta=x.childNodes[3].childNodes[1];//экран
-	const pred=x.childNodes[1];//кнопка лево
-	const next=x.childNodes[5];//кнопка право
+	const tape=x.querySelector('.brands__tape');
+	const pred=x.querySelector('.brands__prev');
+	const next=x.querySelector('.brands__next');
 	const step=146+((3273-146*15)/14);
-	const ip=`translateX(-${step * 5}px)`;
-	lenta.style.transform = ip;//ип
+	const ip='translateX(-'+step * 5+'px)';
+	tape.style.transform = ip; // ip
 	let a=0;
 
-	const f=(e)=>{ // плавно перемещаю кадр
-		lenta.style.transition = 'transform 500ms';
-		lenta.style.transform = `translateX(-${step * (5+e)}px)`;
+	const f=(e)=>{ // slowly move
+		tape.style.transition = 'transform 500ms';
+		tape.style.transform = 'translateX(-'+step * (5+e)+'px)';
 		a=e;
 		}
 
 	pred.onclick=()=>{
 		if(a===-5){
-			lenta.style.transition = '';
-			lenta.style.transform = ip;
+			tape.style.transition = '';
+			tape.style.transform = ip;
 			a=0;
-			setTimeout(()=>{f(-1)},0);// телепортирую кадр
+			setTimeout(()=>{f(-1)},0);// fast
 			return};
 		if(a>-5)f(a-1)};
 	next.onclick=()=>{
 		if(a===5){
-			lenta.style.transition = '';
-			lenta.style.transform = ip;
+			tape.style.transition = '';
+			tape.style.transform = ip;
 			a=0;
-			setTimeout(()=>{f(+1)},0);// телепортирую кадр
+			setTimeout(()=>{f(+1)},0);// fast move
 			return};
 		if(a<5)f(a+1)};
 }
 
-feb2(megaslider);
+slider(megaslider);
 })()
 
 
